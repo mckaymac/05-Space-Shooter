511cf851678c929dd90ea39eb53daea158cc02d0
mckaymac <mckaymac@iu.edu>
2019-10-05 22:53:28 -0400

    updates main.py

--- main.py
+++ main.py
@@ -115,64 +115,6 @@ class Window(arcade.Window):
             bullet = Bullet((x,y),(0,10),BULLET_DAMAGE)
             self.bullet_list.append(bullet)
 
-    # def __init__(self, width, height, title):
-
-    #     # Call the parent class's init function
-    #     super().__init__(width, height, title)
-
-    #     # Make the mouse disappear when it is over the window.
-    #     # So we just see our object, not the pointer.
-    #     self.set_mouse_visible(False)
-
-    #     arcade.set_background_color(open_color.black)
-
-
-
-    # def setup(self):
-    #     pass 
-
-    # def update(self, delta_time):
-    #     pass
-
-    # def on_draw(self):
-    #     """ Called whenever we need to draw the window. """
-    #     arcade.start_render()
-
-
-
-
-    # def on_mouse_motion(self, x, y, dx, dy):
-    #     """ Called to update our objects. Happens approximately 60 times per second."""
-    #     pass
-
-    # def on_mouse_press(self, x, y, button, modifiers):
-    #     """
-    #     Called when the user presses a mouse button.
-    #     """
-    #     pass
-
-    # def on_mouse_release(self, x, y, button, modifiers):
-    #     """
-    #     Called when a user releases a mouse button.
-    #     """
-    #     pass
-
-    # def on_key_press(self, key, modifiers):
-    #     """ Called whenever the user presses a key. """
-    #     if key == arcade.key.LEFT:
-    #         print("Left")
-    #     elif key == arcade.key.RIGHT:
-    #         print("Right")
-    #     elif key == arcade.key.UP:
-    #         print("Up")
-    #     elif key == arcade.key.DOWN:
-    #         print("Down")
-
-    # def on_key_release(self, key, modifiers):
-    #     """ Called whenever a user releases a key. """
-    #     pass
-
-
 def main():
     window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
